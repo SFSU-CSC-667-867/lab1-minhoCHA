cc9edeb13051eebc6d97aae67883b18589242886
minhoCha <minhocha1004@gmail.com>
2019-02-06 20:27:59 -0800

    the second mission completed

--- node/functions.js
+++ node/functions.js
@@ -15,5 +15,15 @@ arrow('hi')
 // for practice
 
 // make an arrow function with no block body
+const myArrow = (y) => y + 1;
+//{
+//  return y + 1;
+//}
+console.log(myArrow(8));
 
 // make a function that returns a function
+const functionFactory = () => () => console.log("This is weied");
+const a = functionFactory();
+a();
+const anotherFunction = functionFactory;
+anotherFunction()();

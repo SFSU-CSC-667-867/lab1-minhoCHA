cc34cd90d6ab3a01fe2504f170c5f987b3880d02
minhoCha <minhocha1004@gmail.com>
2019-02-06 20:39:17 -0800

    the third mission completed

--- node/arrays.js
+++ node/arrays.js
@@ -15,15 +15,34 @@ array[1] = 'hello';
 // for practice
 
 // use forEach
+const arrayTest = [9, 3, 4];
+console.log(arrayTest);
+arrayTest.forEach((a) => {
+    console.log(a);
+});
+
+array.forEach((a) => {
+    console.log(a);
+})
 
 // use map
+const mapArray = arrayTest.map((i) => i + 1);
+console.log(mapArray);
 
 // use pop
+mapArray.pop();
+console.log(mapArray);
 
 // use push
+mapArray.push(6);
+console.log(mapArray);
 
 // use shift
+mapArray.shift()
+console.log(mapArray);
 
 // use unshift
+mapArray.unshift(5);
+console.log(mapArray);
 
 // use filter
